40c3ab5dbcadca06df4a10a6e063dd0ff5a18567
Kiersten <kamccomas@uncg.edu>
2026-04-13 20:32:48 -0400

    finish base of JS

--- script.WIP0.js
+++ script.WIP0.js
@@ -27,6 +27,28 @@ function manageItemLimit() {
 
 function startAnimation() {
   if (currentlyPlaying || container.children.length === 0) return;
+  currentlyPlaying = true;
 
+ gsap.to(".item", {
+  y: 1000
+  scale: 0.5
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.025
+  onComplete: function () {
+    this.targets().forEach((item) => {
+      if (item.parentNode) {
+        item.parentNode.removeChild(item);
+      }
+    });
 
+    currentlyPlaying = false;
+  },
+ });
+  }
+});
+container.addEventListener("mousemove", function(event) {
+  clearTimeout(animationTimeout);
+  addNewItem(event.pageX, event.pageY);
+  animationTimeout = setTimeout(startAnimation, 100);
 });
